f4feeb0cbd677fc64ee948dec8f65354a9fc7298
Lingbo Meng <95656493+Mengx207@users.noreply.github.com>
2023-03-08 13:17:40 -0500

    new file:   .vscode/settings.json 	modified:   CMakeFiles/chessboard.dir/chessboard.cpp.o 	modified:   chessboard 	modified:   chessboard.cpp 	renamed:    image_captured.png -> image_captured1.png 	new file:   image_captured2.png

--- chessboard.cpp
+++ chessboard.cpp
@@ -10,11 +10,12 @@ vector<Point3f> createBoardPoints(Size2i board_shape, double diagonal_spacing);
 
 int main(int argc, char ** argv)
 {
-    Mat gray = imread( "image_captured.png", IMREAD_GRAYSCALE);
+    Mat image_dot = imread( "image_captured1.png", IMREAD_GRAYSCALE);
     // Mat gray = imread( "symmetric_dots.png", IMREAD_GRAYSCALE);
-    Mat draw_img(gray.rows, gray.cols, IMREAD_GRAYSCALE);
+    Mat image_dot_center(image_dot.rows, image_dot.cols, IMREAD_GRAYSCALE);
+    Mat board_points(1000, 1000, IMREAD_GRAYSCALE);
 
-    if( gray.empty()){
+    if( image_dot.empty()){
         cout << "Error opening image" << endl;
         return EXIT_FAILURE;
     }
@@ -24,21 +25,52 @@ int main(int argc, char ** argv)
     params.maxArea = 10e4;
     // Ptr<FeatureDetector> blobDetector = new SimpleBlobDetector(params);
     Ptr<FeatureDetector> blobDetector = SimpleBlobDetector::create(params);
-    bool patternfound = findCirclesGrid(gray,patternsize,centers, CALIB_CB_ASYMMETRIC_GRID, blobDetector);
-    // cout<<centers<<endl;
-    // cout<<centers.size()<<endl;
-    drawChessboardCorners(draw_img, patternsize, Mat(centers), patternfound);
+    bool patternfound = findCirclesGrid(image_dot,patternsize,centers, CALIB_CB_ASYMMETRIC_GRID, blobDetector);
+    cout<<centers<<endl<<endl;
+    drawChessboardCorners(image_dot_center, patternsize, Mat(centers), patternfound);
 
     Size2i board_shape(5,11);
     double diagonal_spacing = 9;
     vector<Point3f>boardPoints = createBoardPoints(board_shape, diagonal_spacing);
-    cout<<boardPoints<<endl<<boardPoints.size()<<endl;
+    vector<Point2f>boardPoints_2D;
+    for(int n=0; n<boardPoints.size(); n++)
+    {
+        boardPoints_2D.push_back(Point2f(10*boardPoints[n].x+400,-(10*boardPoints[n].y)+200)); // to show the target board feature dot, reverse y axis, zoom and shift to center of image
+    }
+    drawChessboardCorners(board_points, board_shape, Mat(boardPoints_2D), patternfound);
+    cout<<boardPoints<<endl<<boardPoints.size()<<endl<<boardPoints_2D<<endl;
 
-    imshow("Captured Image", gray);    // Show the result
-    imshow("Found Centers", draw_img);
+    Mat cameraMatrix(3,3,IMREAD_GRAYSCALE), distCoeffs(1,5,IMREAD_GRAYSCALE);
+    cameraMatrix = ( 3.4714076499814091e+03, 0., 7.5181741352412894e+02, 
+                    0., 3.4711767048332676e+03, 5.4514783904300646e+02, 
+                    0., 0., 1. );
+    distCoeffs = ( -1.8430923287702131e-01, -4.2906853550556068e-02, -2.1393762247926785e-04, 2.9790668148119045e-04, 5.9981578839159733e+00 );
+    Mat rvec, tvec;
+    solvePnP(boardPoints,centers, cameraMatrix, distCoeffs,rvec, tvec);
+    cout<<"rvec:"<<endl<<rvec<<endl<<"tvec:"<<endl<<tvec<<endl;
+
+    imshow("Captured Image", image_dot);    // Show the result
+    imshow("Captured Image Centers", image_dot_center);
+    imshow("Board Points", board_points);
     waitKey();
     return EXIT_SUCCESS;
 }
+
+// vector<Point3f> createBoardPoints(Size2i board_shape, double diagonal_spacing)
+// {
+//     double spacing = diagonal_spacing/sqrt(2);
+//     vector<Point3f> centered_board_points(board_shape.area());
+//     for(int n=0; n<centered_board_points.size(); ++n)
+//     {
+//         int row_n = n/board_shape.width;
+//         int col_n = n%board_shape.width;
+//         centered_board_points[n].x = (float)(2*col_n+row_n%2+0.5-board_shape.width)*spacing;
+//         centered_board_points[n].y = (float)(row_n - (board_shape.height - 1) / 2.) * spacing;
+//         centered_board_points[n].z = 0.0;
+//     } 
+//     return centered_board_points;
+
+// }
 
 vector<Point3f> createBoardPoints(Size2i board_shape, double diagonal_spacing)
 {
@@ -48,8 +80,8 @@ vector<Point3f> createBoardPoints(Size2i board_shape, double diagonal_spacing)
     {
         int row_n = n/board_shape.width;
         int col_n = n%board_shape.width;
-        centered_board_points[n].x = (float)(2*col_n+row_n%2+0.5-board_shape.width)*spacing;
-        centered_board_points[n].y = (float)(row_n - (board_shape.height - 1) / 2.) * spacing;
+        centered_board_points[n].x = (float)(row_n - (board_shape.width - 1) / 2.) * spacing;
+        centered_board_points[n].y = (float)(2*col_n+row_n%2+0.5-board_shape.height)*spacing;
         centered_board_points[n].z = 0.0;
     } 
     return centered_board_points;
